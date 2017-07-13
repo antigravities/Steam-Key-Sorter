ec7abb29f1b3e7df0a57c86198718176a9d1850d
Alexandra Frock <im@alexandra.moe>
2017-07-12 22:38:30 -0400

    Various changes
    * Take key file argument from command line
    * Replace cancerous line endings

--- AutoKeySorter.py
+++ AutoKeySorter.py
@@ -1,12 +1,28 @@
-import re, sys
-filename = "test.txt"
-with open(filename) as f:
-    for line in f:
-        line = line.strip()
-        matches = re.finditer(r"(\w{5}-){2}\w{5}", line) 
-        with open('2.txt', 'a') as f:
-            for match in (matches):
-                sys.stdout = f
-                print ("{match}".format(match = match.group()))
+#!/usr/bin/python
 
-   
+import re, sys
+
+# Defaults
+input_file = "filename.txt";
+output_file = "2.txt";
+
+if len(sys.argv) >= 2:
+    input_file = sys.argv[1];
+
+if len(sys.argv) >= 3:
+    output_file = sys.argv[2];
+
+try:
+    with open(input_file) as f:
+        for line in f:
+            line = line.strip()
+            matches = re.finditer(r"(\w{5}-){2}\w{5}", line)
+            with open(output_file, 'a') as f:
+                for match in (matches):
+                    sys.stdout = f
+                    print ("{match}".format(match = match.group()))
+                f.close();
+        f.close();
+except IOError:
+    print("Could not open " + input_file + ". Does it exist?");
+    print("Hint: if you'd like to specify the file to open, specify it as the first argument.");
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Easily remove unwanted characters, leaving behind steam keys for easy activation.
 
 AutoKeySorter.py - reads the named file and outputs the sorted keys in a new file.
-  - Edit the "test.txt" with your files name, and "2.txt" for the file output name
+  - Usage: `(python) AutoKeySorter.py [input] [output]`
 
 KeySorterManual.py - Manually enter data in place of "Steam Game - xxxxx-xxxxx-xxxxx"
   - Will display the results in the commandline and will exit on keys press.
